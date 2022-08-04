857dc9ff85f9f48c3da84a79dea10b4b141e7296
Shammyshanks <thorinbriggz@gmail.com>
2022-08-04 14:16:29 -0700

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -46,9 +46,9 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-
+fa
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "st"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -238,32 +238,32 @@ globalkeys = gears.table.join(
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
 
-    awful.key({ modkey,           }, "j",
-        function ()
-            awful.client.focus.byidx( 1)
-        end,
-        {description = "focus next by index", group = "client"}
-    ),
-    awful.key({ modkey,           }, "k",
-        function ()
-            awful.client.focus.byidx(-1)
-        end,
-        {description = "focus previous by index", group = "client"}
-    ),
+    --awful.key({ modkey,           }, "j",
+        --function ()
+            --awful.client.focus.byidx( 1)
+        --end,
+        --{description = "focus next by index", group = "client"}
+    --),
+    --awful.key({ modkey,           }, "k",
+        --function ()
+            --awful.client.focus.byidx(-1)
+        --end,
+        --{description = "focus previous by index", group = "client"}
+    --),
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
-              {description = "swap with next client by index", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
-              {description = "swap with previous client by index", group = "client"}),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
-              {description = "focus the next screen", group = "screen"}),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
-              {description = "focus the previous screen", group = "screen"}),
-    awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
-              {description = "jump to urgent client", group = "client"}),
+--    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
+--              {description = "swap with next client by index", group = "client"}),
+--    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
+--              {description = "swap with previous client by index", group = "client"}),
+--    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+--              {description = "focus the next screen", group = "screen"}),
+--    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+--              {description = "focus the previous screen", group = "screen"}),
+--    awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
+--              {description = "jump to urgent client", group = "client"}),
     awful.key({ modkey,           }, "Tab",
         function ()
             awful.client.focus.history.previous()
@@ -280,10 +280,25 @@ globalkeys = gears.table.join(
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
+   
+        -- By direction client focus
+    awful.key({ modkey,           }, "j", function() awful.client.focus.global_bydirection("down")
+        if client.focus then client.focus:raise() end end,
+        {description = "Focus down", group = "client"}),
+    awful.key({ modkey,           }, "k", function() awful.client.focus.global_bydirection("up")
+        if client.focus then client.focus:raise() end end,
+        {description = "Focus up", group = "client"}),
+    awful.ke({ modkey,           }, "h", function() awful.client.focus.global_bydirection("left")
+        if client.focus then client.focus:raise() end end,
+        {description = "Focus left", group = "client"}),
+    awful.key({ modkey,           }, "l", function() awful.client.focus.global_bydirection("right")
+        if client.focus then client.focus:raise() end end,
+        {description = "Focus right", group = "client"}),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
+    
+    --awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
               {description = "increase master width factor", group = "layout"}),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
+    --awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
               {description = "decrease master width factor", group = "layout"}),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
               {description = "increase the number of master clients", group = "layout"}),
@@ -562,3 +577,8 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Auto Start
+awful.spawn.with_shell("picom")
+awful.spawn.with_shell("emacs --fg-daemon")
+awful.spawn.with_shell()
